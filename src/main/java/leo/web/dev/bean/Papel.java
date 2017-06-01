026afdd97660a779bfe563643f26f94070af1420
leonardoBrendo <leonardoBrendo@hotmail.com>
2017-06-01 17:01:40 -0300

    alteração final do primeiro commit

--- src/main/java/leo/web/dev/bean/Papel.java
+++ src/main/java/leo/web/dev/bean/Papel.java
@@ -1,10 +1,14 @@
 package leo.web.dev.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Papel {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String funcao;
 	
