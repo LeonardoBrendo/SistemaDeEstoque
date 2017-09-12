4c75587f4c7c130a71e69a48da3bb0caaeb44f94
teste <teste@192.168.1.3>
2017-09-12 18:13:32 -0300

    Adicionar index de cliente e produtos

--- src/main/java/leo/web/dev/controller/ClienteController.java
+++ src/main/java/leo/web/dev/controller/ClienteController.java
@@ -1,11 +1,12 @@
 package leo.web.dev.controller;
 
-import javax.validation.Valid;
+import java.util.List;
 
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,25 +15,38 @@ import leo.web.dev.bean.Cliente;
 import leo.web.dev.service.ClienteService;
 
 @Controller
-@RequestMapping(path = "/cadastro-cliente/")
+@RequestMapping(path = "/indexCliente/")
 public class ClienteController {
 	@Autowired
 	ClienteService clienteService;
 
-	@RequestMapping("/")
-	public ModelAndView cliente(Cliente cliente) {
-		ModelAndView modelAndView = new ModelAndView("cadastro-cliente");
-		return modelAndView;
+	@RequestMapping(path="/")
+	public ModelAndView index(Cliente cliente){
+		ModelAndView model= new ModelAndView("/cliente/indexcliente");
+		List<Cliente> clientes = clienteService.findAllCliente();
+		model.addObject("clientes", clientes);
+		return model;
+	}
+	@RequestMapping(path="/paginaSalvar")
+	public ModelAndView paginaSalvarCliente(Cliente cliente){
+		ModelAndView model= new ModelAndView("/cliente/cadastro-cliente");
+		return model;
 	}
 
-	@RequestMapping(path = "/salvar", method = RequestMethod.POST)
-	public ModelAndView salvar(@Valid Cliente cliente, BindingResult result, RedirectAttributes attributes) {
-		if (result.hasErrors()) {
-			return cliente(cliente);
-		}
+	@RequestMapping(path = "/salvar", method=RequestMethod.POST)
+	public ModelAndView salvar(@Valid Cliente cliente, RedirectAttributes attributes) {		
 		clienteService.salvarCliente(cliente);
-		attributes.addFlashAttribute("mensagem", "Cliente cadastrado");
-		return new ModelAndView("redirect:/cadastro-cliente/");
+		attributes.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso");
+		ModelAndView model = new ModelAndView("/cliente/indexcliente"); 
+		return model;
 	}
 
+	@RequestMapping("/listar")
+	public String listaClientes(Model model) {
+		List<Cliente> listaClientes = clienteService.findAllCliente();	
+		if (listaClientes != null) {
+			model.addAttribute("clientes", listaClientes);
+		}
+		return "/cliente/listaDeClientes";
+	}
 }
